425b3cc91503291e3858378a7ba0175c6dfc2c3d
AnshuGupta7 <anshugupta.korba@gmail.com>
2016-08-16 17:30:40 +0530

    Replace Logic implemented

--- src/main/java/com/yodlee/jsonEditor/Utils/SpecificKeySearch.java
+++
@@ -1,9 +0,0 @@
-package com.yodlee.jsonEditor.Utils;
-
-/**
- * Created by agupta5 on 26-06-2016.
- */
-public class SpecificKeySearch {
-
-
-}
--- src/main/java/com/yodlee/jsonEditor/methods/InputPathParser.java
+++ src/main/java/com/yodlee/jsonEditor/methods/InputPathParser.java
@@ -4,41 +4,11 @@ package com.yodlee.jsonEditor.methods;
  * Created by agupta5 on 27-07-2016.
  */
 public class InputPathParser {
-    public static void parsePath(String str){
-        /*String[] elements= path.split(".");
-        int len=elements.length;
-        for(String str : elements) {
-            if (str.contains("[")){
-                InputPathParser.returnJSONArray(str);
-            }
-            else if(str.equals(elements[len-1])){
-                InputPathParser.returnKey(str);
-            }
-            else
-                InputPathParser.returnJSONObj(str);
-        }*/
-        if (str.contains("[")) {
-            InputPathParser.returnJSONArray(str);
-        }
-
-    }
     public static int isArray(String str){
         int index=-1;
         if (str.contains("[")) {
             index= str.charAt(str.length()-2)-48;
         }
         return index;
-    }
-
-    public static String returnJSONArray(String elem){
-        return elem.substring(0,elem.length()-3);
-    }
-
-    public static String returnKey(String elem){
-        return elem;
-    }
-
-    public static String returnJSONObj(String elem){
-        return elem;
     }
 }
